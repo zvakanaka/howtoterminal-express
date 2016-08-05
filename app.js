91798db4a4534a6b9cf52841b25454170d5ef42b
Adam Quinton <adamquinton@familysearch.org>
2016-08-05 12:15:54 -0600

    Initial working commit

---
+++ app.js
@@ -0,0 +1,14 @@
+var express = require("express");
+//Root app
+var app = express();
+
+//Sub-apps
+var scrape = require("bike-list");
+//var index = require("howtoterminal");
+
+var PORTNO = process.env.PORT || 5000;
+app.listen(PORTNO);
+console.log(PORTNO + ' is the magic port');
+
+app.use("/scrape", scrape);
+//app.use("/", index);
