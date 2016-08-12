95c3e30ab9ce8860dba4521dec86a2bc341e491c
Adam Quinton <adamquinton@familysearch.org>
2016-08-12 16:06:43 -0600

    photo gallery and global nav

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require("express");
 //Root app
 var app = express();
+// var gallery = require('node-gallery')
 
 //Sub-apps
 var scrape = require("bike-list");
@@ -11,4 +12,11 @@ app.listen(PORTNO);
 console.log(PORTNO + ' is the magic port');
 
 app.use("/scrape", scrape);
+app.use('/photo', require('node-gallery')({
+  staticFiles : process.env.GALLERY_DIR || 'photo',
+  urlRoot : 'photo',
+  title : 'Gallery',
+  image: {height: '50%', width: '50%'}
+}));
+// app.use('/', express.static(process.env.STATIC_FILES || __dirname+'/public'));
 app.use("/", index);
