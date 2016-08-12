78cdd9336c8d81a66506193e962c95fcf68c9049
Adam Quinton <adamquinton@familysearch.org>
2016-08-12 11:28:33 -0600

    Add howtoterminal home page

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var app = express();
 
 //Sub-apps
 var scrape = require("bike-list");
-//var index = require("howtoterminal");
+var index = require("howtoterminal");
 
 var PORTNO = process.env.PORT || 5000;
 app.listen(PORTNO);
 console.log(PORTNO + ' is the magic port');
 
 app.use("/scrape", scrape);
-//app.use("/", index);
+app.use("/", index);
